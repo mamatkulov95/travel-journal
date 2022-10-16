fbb3b87638d50809e719bf4a0ac86758f77b9308
M_Coder <m.mamatkulov95@gmail.com>
2022-10-16 21:04:36 +0500

    Card section finished using map() method and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Card from './card-section/Card'
+import data from '../src/card-section/CardData'
 
 function App() {
+  const travelCountry = data.map((item)=>{
+    return(
+      <Card 
+      key={item.id}
+      {...item}
+      
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <Card />
+      {travelCountry}
     </div>
   );
 }
--- src/card-section/Card.js
+++ src/card-section/Card.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Card.css";
+import locationLogo from "../images/location-logo.png"
 
-export default function Card(){
-    return(
-        <h1>Test from Card js</h1>
-    )
+export default function Card(props) {
+  return (
+    <section className="card-section">
+      <img className="travel-photo" src={props.travelPhoto}></img>
+      <div className="travel-content">
+        <div className="travel-location">
+          <img className="location-logo" src={locationLogo}></img>
+          <p className="travel-country">{props.travelCountry}</p>
+          <a className="travel-map"  target="_blank" href={props.travelUrl}>View on Google Maps </a>
+        </div>
+        <h1 className="travel-place">{props.travelPlace}</h1>
+        <p className="travel-date">{props.travelDate}</p>
+        <p className="travel-place-description">{props.travelDescription}</p>
+      </div>
+    </section>
+  );
 }
